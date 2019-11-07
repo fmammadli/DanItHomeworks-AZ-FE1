2b9553d47f0708f65eedc688e805bf19fc25e5f7
fmammadli <56446918+fmammadli@users.noreply.github.com>
2019-11-07 22:40:14 +0400

    Update script.js

--- javascript/homework9/js/script.js
+++ javascript/homework9/js/script.js
@@ -1,9 +1,36 @@
-$('.tabs-title').not('.active').click(function(){
-    let index = $(this).index();
-    let content = $('.tabs-content-item').eq(index);
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.tabs-content-item').removeClass('active').eq(index).addClass('active');
-});
+let tabs;
+let content;
+function ready () {
+    tabs = document.querySelectorAll('.tabs-title');
+    content = document.querySelectorAll('.tabs-cont');
+    hideTabsContent(1);
+};
 
-$('.tabs-title:first').addClass('active');
-$('.tabs-content-item:first').addClass('active');
+function hideTabsContent(a) {
+    for (let i = a; i < content.length; i++){
+        content[i].classList.remove('show');
+        content[i].classList.add('hide');
+        tabs[i].classList.remove('active')
+    }
+}
+
+document.getElementById('btns').onclick = function (event) {
+    let target = event.target;
+    for (let i = 0; i < tabs.length; i ++) {
+        if (target === tabs[i]) {
+            showTabsContent (i);
+            break;
+        }
+    }
+};
+function showTabsContent (b) {
+    if (content[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tabs[b].classList.add('active');
+        content[b].classList.remove('hide');
+        content[b].classList.add('show');
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", ready);
